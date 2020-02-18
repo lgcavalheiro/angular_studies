f6e263debdbc02bc43ce940ff2cf1f13e60fb00b
lgcavalheiro <lg.cavalheiro@hotmail.com>
2020-02-18 23:42:25 +0000

    studies 18-02-2020

--- my-first-app/src/app/welcome/welcome.component.ts
+++ my-first-app/src/app/welcome/welcome.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-welcome',
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomeComponent implements OnInit {
 
-  constructor() { }
+  @Input() name: string;
+
+  constructor() {
+
+  }
 
   ngOnInit(): void {
+    this.setUpperCase();
+    console.log(this.name);
+  }
+
+  setUpperCase() {
+    this.name = this.name.toUpperCase();
+  }
+
+  displayName() {
+    alert(this.name);
   }
 
 }
